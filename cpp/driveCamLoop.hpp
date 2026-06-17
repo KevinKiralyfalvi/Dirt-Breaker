63337394f4a57a9f9201fac17cd7c1031fdcaa5a
Kevin Kiralyfalvi <kevin.kiralyfalvi@gmail.com>
2026-06-17 11:37:02 -0500

    remove erroneous file

--- cpp/driveCamLoop.hpp
+++
@@ -1,7 +0,0 @@
-#include "convert888MatrixTo565Array.hpp"
-#include "writeFrame.hpp"
-#include <iostream>
-#include <opencv2/opencv.hpp>
-#include <opencv2/videoio.hpp>
-
-int driveCamLoop(cv::VideoCapture driveCamera);
